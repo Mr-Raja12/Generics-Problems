a30231c224df2a3ecb43ae7775f06ee390b6528b
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-08 02:13:31 +0530

    Added: Find the Maximum Number Among Three Integer Using compareTo method

--- src/com/bl/generics/problems/GenericsTestMaximum.java
+++ src/com/bl/generics/problems/GenericsTestMaximum.java
@@ -2,40 +2,56 @@ package com.bl.generics.problems;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Raja To find the max integer among three integer using compareTo
+ *         method
+ *
+ */
 public class GenericsTestMaximum {
-        
+	/**
+	 * @param main method to call the compareTo method
+	 */
+
 	public static void main(String[] args) {
-		Integer firstNumber, secoundNumber,thirdNumber;
+		Integer firstNumber, secoundNumber, thirdNumber;
 		System.out.println("Enter the three integer");
-		Scanner scanner= new Scanner(System.in);
-		firstNumber=scanner.nextInt();
-		secoundNumber=scanner.nextInt();
-		thirdNumber=scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		firstNumber = scanner.nextInt();
+		secoundNumber = scanner.nextInt();
+		thirdNumber = scanner.nextInt();
 		System.out.println("Maximum among three integer");
 		GenericsTestMaximum max = new GenericsTestMaximum();
 		max.findMax(firstNumber, secoundNumber, thirdNumber);
-		
+
 	}
-	
-public void findMax(Integer firstNumber, Integer secoundNumber, Integer thirdNumber) {
-	
-	Integer maxNumber;
-	
-	if(firstNumber.compareTo(secoundNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0 )
-	{
-		maxNumber = firstNumber;
-	}
-	
-	else if(secoundNumber.compareTo(firstNumber) > 0 && secoundNumber.compareTo(thirdNumber) > 0 )
-	{
-		maxNumber = secoundNumber;
-	}
-	
-	else 
-		{
-		maxNumber = thirdNumber;
+
+	/**
+	 * Created a method to initialize the three integer
+	 * 
+	 * @param firstNumber
+	 * @param secoundNumber
+	 * @param thirdNumber
+	 */
+	public void findMax(Integer firstNumber, Integer secoundNumber, Integer thirdNumber) {
+
+		Integer maxNumber;
+		/*
+		 * Checking the condition b/w three value
+		 */
+		if (firstNumber.compareTo(secoundNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+			maxNumber = firstNumber;
 		}
-	System.out.println("Maximum integer is : " + maxNumber + " among " + firstNumber + ", " + secoundNumber + " and " + thirdNumber);
+
+		else if (secoundNumber.compareTo(firstNumber) > 0 && secoundNumber.compareTo(thirdNumber) > 0) {
+			maxNumber = secoundNumber;
+		}
+
+		else {
+			maxNumber = thirdNumber;
+		}
+		System.out.println("Maximum integer is : " + maxNumber + " among " + firstNumber + ", " + secoundNumber
+				+ " and " + thirdNumber);
 	}
-	
+
 }
