2a6bc8fb65741d2ba9b17653eb477675937c04b5
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-08 03:32:34 +0530

    Added: Find the Max String among three string using compareTo method

--- src/com/bl/generics/problems/GenericsTestMaximum.java
+++ src/com/bl/generics/problems/GenericsTestMaximum.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 
 /**
  * 
- * @author Raja To find the max integer among three integer using compareTo
- *         method
+ * @author Raja 
+ * 1)To find the max integer among three integer using compareTo
+ * method 
+ * 2)To find the max integer among three String using compareTo
+ *  method
  *
  */
 public class GenericsTestMaximum {
-	 /**
+	/**
 	 * @param main method to call the compareTo method
 	 */
 	public static void main(String[] args) {
 		Integer firstNumber, secoundNumber, thirdNumber;
+		String firstString, secoundString, thirdString;
 		System.out.println("Enter the three integer");
 		Scanner scanner = new Scanner(System.in);
 		firstNumber = scanner.nextInt();
 		secoundNumber = scanner.nextInt();
 		thirdNumber = scanner.nextInt();
+		System.out.println("Enter the three String");
+		firstString = scanner.nextLine();
+		secoundString = scanner.nextLine();
+		thirdString = scanner.nextLine();
+
 		System.out.println("Maximum among three integer");
 		GenericsTestMaximum max = new GenericsTestMaximum();
 		max.findMax(firstNumber, secoundNumber, thirdNumber);
+		max.findMax(firstString, secoundString, thirdString);
 
 	}
 
@@ -51,6 +61,32 @@ public class GenericsTestMaximum {
 		}
 		System.out.println("Maximum integer is : " + maxNumber + " among " + firstNumber + ", " + secoundNumber
 				+ " and " + thirdNumber);
+	}
+
+	/**
+	 * Created a method to initialize the three String
+	 * 
+	 * @param firstString
+	 * @param secoundString
+	 * @param thirdString
+	 */
+	public void findMax(String firstString, String secoundString, String thirdString) {
+
+		String maxString;
+
+		if (firstString.compareTo(secoundString) > 0 && firstString.compareTo(thirdString) > 0) {
+			maxString = firstString;
+		}
+
+		else if (secoundString.compareTo(firstString) > 0 && secoundString.compareTo(thirdString) > 0) {
+			maxString = secoundString;
+		}
+
+		else {
+			maxString = thirdString;
+		}
+		System.out.println("Maximum String is : " + maxString + " among " + firstString + ", " + secoundString + " and "
+				+ thirdString);
 	}
 
 }
